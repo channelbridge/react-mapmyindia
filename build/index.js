e391cd2dcdd358c25bde22dc7db97f47742e91e6
shubhang <shubhang@channelbridge.in>
2017-11-26 09:33:14 +0530

    add source

---
+++ build/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.MapMarker = undefined;
+
+var _MapMyIndia = require('./MapMyIndia');
+
+Object.defineProperty(exports, 'MapMarker', {
+  enumerable: true,
+  get: function get() {
+    return _MapMyIndia.MapMarker;
+  }
+});
+
+var _MapMyIndia2 = _interopRequireDefault(_MapMyIndia);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _MapMyIndia2.default;
